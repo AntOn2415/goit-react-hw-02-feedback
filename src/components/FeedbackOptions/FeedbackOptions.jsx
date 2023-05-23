7d0f67fd4cf70d2b0d7415423e8c0f6592fbaa45
AntOnAntOff <povanton@gmail.com>
2023-05-24 00:40:09 +0300

    new

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import css from './FeedbackOptions.module.css';
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <div className={css.options}>
+      {options.map(option => (
+          <button className={css.optionsBtn} key={option} onClick={() => onLeaveFeedback(option)}>
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string),
+  onLeaveFeedback: PropTypes.func,
+};
+
+
+export default FeedbackOptions;
---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import css from './Section.module.css'
+
+const Section = ({ title, children }) => {
+  return (
+    <>
+      <h2 className={css.sectionTitle}>{title}</h2>
+      {children}
+    </>
+  );
+};
+
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+}
+export default Section;
